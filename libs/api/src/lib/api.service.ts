27badb9c3baa3420d9d68b1b0eaf8c36f68e1346
Raoul_AHOUANSOU <ahouansouraoul@gmail.com>
2021-06-04 18:59:42 +0100

    changement des urls environnement2

--- libs/api/src/lib/api.service.ts
+++ libs/api/src/lib/api.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 // import { environment } from '@env/environment';
-import { environment } from '../../../../environments/environment';
+import { environment } from '@env/environment';
 
 @Injectable()
 export class ApiService {
